71653c9d5fa414e65cb70c35f03470c6c9b86b74
Komissarikhin-S <komissarihin.s@yandex.ru>
2021-11-19 14:56:49 +0300

    cosmetic

--- src/main/java/web/model/CreateRoleAndUsers.java
+++ src/main/java/web/model/CreateRoleAndUsers.java
@@ -2,7 +2,6 @@ package web.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.thymeleaf.expression.Sets;
 import web.service.RoleService;
 import web.service.UserService;
 
